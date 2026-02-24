4aee52f91164c992ecb1b0a4f5c34c0cff9c9237
Victor Schmidt <s234944@dtu.dk>
2026-02-24 13:54:55 +0100

    Changed model to simpler and ran it

--- AMLA_Project_1/model.py
+++ AMLA_Project_1/model.py
@@ -5,43 +5,34 @@ class Model(nn.Module):
     def __init__(self,
                  in_channels=1,
                  num_classes=24,
-                 c1=16, c2=32, c3=64, c4=128,
-                 fc1=128, fc2=32,
+                 c1=8, c2=16, c3=32,
+                 fc1=64,
                  dropout=0.2):
         super().__init__()
 
         self.net = nn.Sequential(
             nn.Conv2d(in_channels, c1, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(c1),
+            nn.Dropout2d(p=dropout),
             nn.MaxPool2d(kernel_size=2),
 
             nn.Conv2d(c1, c2, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(c2),
+            nn.Dropout2d(p=dropout),
             nn.MaxPool2d(kernel_size=2),
 
             nn.Conv2d(c2, c3, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.BatchNorm2d(c3),
-            nn.MaxPool2d(kernel_size=2),
-
-            nn.Conv2d(c3, c4, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(c4),
+            nn.Dropout2d(p=dropout),
             nn.MaxPool2d(kernel_size=2),
 
             nn.Flatten(),
 
-            nn.LazyLinear(fc1),
+            nn.Linear(2048, fc1),   # avoids hardcoding flatten size
             nn.ReLU(),
             nn.Dropout(p=dropout),
 
-            nn.Linear(fc1, fc2),
-            nn.ReLU(),
-            nn.Dropout(p=dropout),
-
-            nn.Linear(fc2, num_classes)
+            nn.Linear(fc1, num_classes)
         )
 
     def forward(self, x):
